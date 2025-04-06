96abe3bb4b6624f06a6b2977d37c5f540cfe0afb
Rickson Henrique (Xucacodev) <88748126+Henriiquerick@users.noreply.github.com>
2025-04-06 17:58:40 -0300

    add battle, diary, main and utils js

---
+++ main.js
@@ -0,0 +1,22 @@
+import Phaser from 'phaser';
+import GameScene from './game';
+
+const config = {
+  type: Phaser.AUTO,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false,
+    },
+  },
+  scene: [GameScene],
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+};
+
+const game = new Phaser.Game(config);
